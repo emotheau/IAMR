924fe0bf00e1652a6374d7d9b73df6b1e8eabc72
Emmanuel <emotheau@lbl.gov>
2021-01-23 23:34:53 -0800

    WIP: after an intense struggle, we can now copy a multifab to a Torch tensor, but we have to figure out the correct index

--- Source/NS_Torch.cpp
+++ Source/NS_Torch.cpp
@@ -12,11 +12,34 @@ NavierStokesBase::test_libtorch()
 
 {
 
-std::cout << "\n WE ARE IN TEST_LIBTORCH ROUTINE \n";
+  std::cout << "\n WE ARE IN TEST_LIBTORCH ROUTINE \n";
 
 
-  torch::Tensor tensor = torch::eye(3);
-  std::cout << tensor << std::endl;
+  MultiFab& Snew   = get_old_data(State_Type);
+  torch::Tensor t1 = torch::zeros({8,8});
+  std::cout << "Tensor from array:\n" << t1 << '\n';
+
+#ifdef _OPENMP
+#pragma omp parallel if (Gpu::notInLaunchRegion())
+#endif
+  for (MFIter mfi(Snew,TilingIfNotGPU()); mfi.isValid(); ++mfi)
+  {
+    const Box& bx = mfi.tilebox();
+    auto const& Snew_vel = Snew.array(mfi,Xvel);
+
+    amrex::ParallelFor(bx, [t1, Snew_vel]
+    AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+    {
+
+      t1[j][i] = Snew_vel(i,j,k);  // Inverting i and j index because of the PyTorch formalism
+      amrex::Print() << "i= " << i << " j= " << j << "   array= "  << Snew_vel(i,j,k) << std::endl;
+      amrex::Print() << "i= " << i << " j= " << j << "   array Torch= "  << t1[i][j] << std::endl;
+    });
+
+  }
+
+std::cout << "Tensor from array:\n" << t1 << '\n';
+
 
 
 }
